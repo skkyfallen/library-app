1d2c30a23b9328fcc2b4e5f6da037d8edb4cfd89
Ekwonu Kenechukwu <kenzemadrt@gmail.com>
2023-08-22 08:35:36 +0100

    fixed ui

--- src/Views/Auth/SignUp.js
+++ src/Views/Auth/SignUp.js
@@ -5,6 +5,7 @@ import { BiSolidRightArrowCircle } from "react-icons/bi";
 import ClipLoader from "react-spinners/ClipLoader";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
 const SignUp = () => {
   const [loading, setloading] = React.useState(false);
@@ -49,63 +50,72 @@ const SignUp = () => {
   };
   return (
     <div className="h-screen bg-libraryOrange flex items-center justify-center font-bold ">
-      <form className="bg-libraryOrange shadow-lg rounded-2xl   w-7/12  flex flex-row justify-between mb-10">
+      <form className="bg-libraryOrange shadow-lg rounded-2xl   w-7/12  flex flex-row justify-between sm:h-4/6">
         <div>
-          <h1 className="text-white font-regular text-3xl flex p-10">
+          <h1 className="text-white font-regular text-3xl flex p-10 sm:p-2 sm:texl-xl">
             Get Started with
           </h1>
-          <h1 className="text-white text-4xl p-10 ">Library - Manager</h1>
-          <p className="text-white text-xl px-10">
+          <h1 className="text-white text-4xl p-10 sm:p-3 sm:text-2xl">Library - Manager</h1>
+          <p className="text-white text-xl px-10 sm:px-3 sm:py-20 ">
             Your all in one Library Assistant
           </p>
-          <p className="text-white px-10 text-2xl py-32">
+          <p className="text-white xl:px-10 xl:text-2xl xl:py-20 sm:px-3 sm-text-xl">
             To manage all your priceless books
             <br />, track and maintain them
+          </p>
+          <p>
+            <NavLink
+              to="/login"
+              className="text-black xl:p-10 hover:text-white text-lg sm:p-3"
+            >
+              Already Have an account?
+            </NavLink>{" "}
           </p>
           <div className="border-2 border-white ml-5 mr-2"></div>
         </div>
         <div className="bg-white w-7/12 rounded-xl">
-          <h1 className="text-black font-bold text-3xl p-10">Sign Up</h1>
-          <p className="p-5">Library Name</p>
+          <h1 className="text-black font-bold xl:text-3xl pt-10 pl-10 sm:pt-2 sm:pl-2 sm:text-2xl">Sign Up</h1>
+          <p className="xl:p-5 sm:p-2 ">Library Name</p>
           <input
             type="text"
             value={libraryName}
             onChange={(event) => setLibraryName(event.target.value)}
             placeholder="Enter your first name"
-            className="p-5 ml-5 w-8/12  h-14 rounded-lg bg-slate-100"
+            className="p-5 ml-5 w-8/12 sm:h-10 xl:h-14  rounded-lg bg-slate-100" 
           />
-          <p className="p-5">Email-address</p>
+          <p className="xl:p-5 sm:p-2">Email-address</p>
           <input
             type="email"
             value={email}
             onChange={(event) => setemail(event.target.value)}
             placeholder="example@email.com"
-            className="p-5 ml-5 w-8/12  h-14 rounded-lg bg-slate-100"
+            className="xl:p-5 sm:p-2 ml-5 w-8/12 sm:h-10 xl:h-14 rounded-lg bg-slate-100"
           />
-          <p className="p-5">Phone-Number</p>
+          <p className="xl:p-5 sm:p-2">Phone-Number</p>
           <input
             type="text"
             value={phone}
             onChange={(event) => setphone(event.target.value)}
             placeholder="Enter your phone number"
-            className="p-5 ml-5 w-8/12  h-14 rounded-lg bg-slate-100"
+            className="xl:p-5 sm:p-2  ml-5 w-8/12 sm:h-10  xl:h-14 rounded-lg bg-slate-100"
           />
-          <p className="p-5">Password</p>
+          <p className="xl:p-5 sm:p-2">Password</p>
           <input
             type="password"
             value={password}
             onChange={(event) => setpassword(event.target.value)}
             placeholder="Enter your password"
-            className="p-5 ml-5 w-8/12  h-14 rounded-lg bg-slate-100"
+            className="xl:p-5 sm:p-2 ml-5 w-8/12 sm:h-10 xl:h-14 rounded-lg bg-slate-100"
           />
           <input
             type="file"
             accept="image/*"
             onChange={(event) => setImage(event.target.files[0])}
+            className="xl:px-10 sm:px-0 xl:pb-10 sm:pb-0 sm:pt-2"
           />
           {loading === true ? (
             <button>
-              <ClipLoader className="relative left-20 top-3" />
+              <ClipLoader className="relative xl:left-20 sm:left-0 sm:absolute top-3" />
             </button>
           ) : (
             <button onClick={handleContinue}>
